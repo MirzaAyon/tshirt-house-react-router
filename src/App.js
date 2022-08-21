5cc2fb2aaf0471cf9c96edb1e67fa3fa632b3884
MirzaAyon <ayonmirza98@gmail.com>
2022-08-21 16:17:21 +0600

    react router installed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Home from './comonents/Home/Home';
+import Header from './comonents/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Home></Home>
     </div>
   );
