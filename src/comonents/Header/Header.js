c15bd92c892de5f6f05115dfcbb0097449822780
MirzaAyon <ayonmirza98@gmail.com>
2022-08-21 16:31:48 +0600

    simple router implemented

--- src/comonents/Header/Header.js
+++ src/comonents/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
     return (
@@ -8,6 +9,7 @@ const Header = () => {
             <nav>
                 <Link to="/home">Home</Link>
                 <Link to="/orderreview">Order Review</Link>
+                {/* home and order review btn ke inspact korle bujha jabe era ashole <a></a> but Link er vitore ache  */}
             </nav>
         </div>
     );
