1a711293f5dad98fce312d882bcbf5cd5b027f6e
MirzaAyon <ayonmirza98@gmail.com>
2022-08-21 16:26:24 +0600

    home and order review btn done

--- src/comonents/Header/Header.js
+++ src/comonents/Header/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
             <h2>Welcome to tshirt house</h2>
             <nav>
-
+                <Link to="/home">Home</Link>
+                <Link to="/orderreview">Order Review</Link>
             </nav>
         </div>
     );
