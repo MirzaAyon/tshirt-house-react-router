faf7c130df8ecb28253b19b2462af6957cf88185
MirzaAyon <ayonmirza98@gmail.com>
2022-08-21 19:48:50 +0600

    conditional rendering first step done

--- src/comonents/Cart/Cart.js
+++ src/comonents/Cart/Cart.js
@@ -2,9 +2,21 @@ import React from 'react';
 import './Cart.css'
 
 const Cart = ({ cart, handleRemoveFromCart }) => {
+    //conditional rendering options
+    //1.element variable
+    let command;
+    if (cart.length === 0) {
+        command = <p>Please add atleast one  items</p>
+    }
+    else if (cart.length === 1) {
+        command = <p>Please add more</p>
+    } else {
+        command = <p><small>Thanks for adding item</small></p>
+    }
     return (
         <div>
             <h2>Iten selected: {cart.length}</h2>
+            {command}
             {
                 cart.map(tshirt => <p>
                     {tshirt.name}
