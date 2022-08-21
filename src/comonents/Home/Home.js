ea017f53745e14351ed6494170a35362d6578a81
MirzaAyon <ayonmirza98@gmail.com>
2022-08-21 16:51:10 +0600

    tshirts link showing on ui

--- src/comonents/Home/Home.js
+++ src/comonents/Home/Home.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import useTshirt from '../../hooks/useTshirts';
 import Cart from '../Cart/Cart';
 import './Home.css'
 
 const Home = () => {
+    const [tshirts, setTshirts] = useTshirt();
     return (
         <div className='home-container'>
-            <div className="tshirt-container"></div>
+            <div className="tshirt-container">
+                <h3>{tshirts.length}</h3>
+            </div>
             <div className="cart-container">
                 <Cart></Cart>
             </div>
