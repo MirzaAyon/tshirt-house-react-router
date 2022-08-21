6d1289a24841139ce7b95a109e732af796e7e150
MirzaAyon <ayonmirza98@gmail.com>
2022-08-21 19:56:11 +0600

    2nd conditional rendering done with ternary operator

--- src/comonents/Cart/Cart.js
+++ src/comonents/Cart/Cart.js
@@ -6,23 +6,29 @@ const Cart = ({ cart, handleRemoveFromCart }) => {
     //1.element variable
     let command;
     if (cart.length === 0) {
-        command = <p>Please add atleast one  items</p>
+        command = <div>
+            <h5>Please add atleast one</h5>
+            <p>You have offers</p>
+        </div>
     }
     else if (cart.length === 1) {
-        command = <p>Please add more</p>
+        command = <p>Please add more </p>
     } else {
         command = <p><small>Thanks for adding item</small></p>
     }
+    //2.Ternary operator
     return (
         <div>
             <h2>Iten selected: {cart.length}</h2>
-            {command}
+
             {
                 cart.map(tshirt => <p>
                     {tshirt.name}
                     <button onClick={() => handleRemoveFromCart(tshirt)}  >X</button>
                 </p>)
             }
+            {command}
+            {cart.length !== 4 ? <p>keep adding</p> : <button>Remove All</button>}
         </div>
     );
 };
