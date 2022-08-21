54d566ffa7025526c82b73db42f04a68b0fd3d48
MirzaAyon <ayonmirza98@gmail.com>
2022-08-21 17:07:37 +0600

    all Tshirts are showing on the UI

--- src/comonents/Home/Home.js
+++ src/comonents/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import useTshirt from '../../hooks/useTshirts';
 import Cart from '../Cart/Cart';
+import Tshirt from '../Tshirt/Tshirt';
 import './Home.css'
 
 const Home = () => {
@@ -8,7 +9,13 @@ const Home = () => {
     return (
         <div className='home-container'>
             <div className="tshirt-container">
-                <h3>{tshirts.length}</h3>
+                {/* <h3>{tshirts.length}</h3> */}
+                {
+                    tshirts.map(tshirt => <Tshirt
+                        key={tshirt._id}
+                        tshirt={tshirt}
+                    ></Tshirt>)
+                }
             </div>
             <div className="cart-container">
                 <Cart></Cart>
