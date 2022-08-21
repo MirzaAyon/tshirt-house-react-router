265fcdf7d3dcb6a270c512595d2933d2b2cf9bd5
MirzaAyon <ayonmirza98@gmail.com>
2022-08-21 16:23:56 +0600

    different routes set and they are accessible

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@
 import './App.css';
 import Home from './comonents/Home/Home';
 import Header from './comonents/Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import OrderReview from './comonents/OrderReview/OrderReview';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <Home></Home>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+
+        <Route path='orderreview' element={<OrderReview></OrderReview>}></Route>
+        <Route></Route>
+
+      </Routes>
     </div>
   );
 }
