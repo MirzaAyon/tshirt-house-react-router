94220504be40fb69ce864df64a3506b6f418a559
MirzaAyon <ayonmirza98@gmail.com>
2022-08-22 12:28:02 +0600

    context api used to pass gold ring successfully

--- src/comonents/GrandPa/Grandpa.js
+++ src/comonents/GrandPa/Grandpa.js
@@ -1,38 +1,46 @@
-import React, { useState } from 'react';
+import React, { createContext, useState } from 'react';
 import Father from '../Father/Father'
 import Uncle from '../Uncle/Uncle'
 import Aunty from '../Aunty/Aunty'
 import './Grandpa.css';
 
+
+export const RingContext = createContext('ring')
 const Grandpa = () => {
     // const house = 7;
     //prothome sudhu 7 diye korbo erpr dynamic bhabe nicher moto krbo
     const [house, sethouse] = useState(1);
 
     const ornament = 'Diamond Ring';
+    const ornament2 = 'gold ring';
     const handleBuyAHouse = () => {
         const newHouseCount = house + 1;
         sethouse(newHouseCount);
     }
+    //context korle kono props pass kora lagbe na 
     return (
-        <div className='grandpa' >
-            <h4>GrandPa</h4>
+        // <RingContext.Provider value='alur-ring'>
+        //chaile hardcoded er bodole evabe dynamic way teo pathaono jae
+        <RingContext.Provider value={ornament2}>
+            <div className='grandpa' >
+                <h4>GrandPa</h4>
 
-            <p>House: {house}
+                <p>House: {house}
 
-                <button onClick={handleBuyAHouse}
-                >Buy a house</button>
+                    <button onClick={handleBuyAHouse}
+                    >Buy a house</button>
 
-            </p>
-            {/* <h2>Grand Pa</h2> */}
-            <section style={{ display: 'flex' }}>
-                <Father house={house} ornament={ornament}></Father>
-                <Uncle house={house}></Uncle>
-                <Aunty house={house}></Aunty>
-            </section>
-            {/* house take shobar jnno use korte chai tai house ke pathae dilam */}
+                </p>
+                {/* <h2>Grand Pa</h2> */}
+                <section style={{ display: 'flex' }}>
+                    <Father house={house} ornament={ornament}></Father>
+                    <Uncle house={house}></Uncle>
+                    <Aunty house={house}></Aunty>
+                </section>
+                {/* house take shobar jnno use korte chai tai house ke pathae dilam */}
 
-        </div>
+            </div>
+        </RingContext.Provider>
     );
 };
 
--- src/comonents/Special/Special.js
+++ src/comonents/Special/Special.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { RingContext } from '../GrandPa/Grandpa';
 
 const Special = ({ ornament }) => {
+    const ring = useContext(RingContext)
+    //grandpa te export kore ekhane import korlam
     return (
         <div>
             <h5>Special</h5>
             <p><small>Gift: {ornament}</small></p>
+            <p><small>Gift: {ring}</small></p>
+            {/* first gift manually 2nd gift with context api  */}
         </div>
     );
 };
 
 export default Special;
 //dada first e father ke erpr father pathaise myself ke and myself pathaise special one ke 
+//kintu eta secret fush kore dichche
