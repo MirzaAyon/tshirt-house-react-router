e075be38a4e485d16d02b0f83718762df399464c
MirzaAyon <ayonmirza98@gmail.com>
2022-08-22 19:48:01 +0600

    to do list added

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ export default App;
 
 
 /*quiz
-5 const array = [“beautiful” , “nice”, “awesome”, ”beauty” ]  
+5 const array = [“beautiful” , “nice”, “awesome”, ”beauty” ]
 const result =  array.includes(“bea”)
 What will be the output of the result?
 ans: You have selected "[ “beautiful” , “beauty” ]" but correct answer is "false".
@@ -50,3 +50,15 @@ ans:You have selected "</Admin>" but correct answer is "</User>".
 
 
 */
+
+
+// to do list 
+// ১. সিম্পল রাউট কিভাবে সেট করে। জাস্ট পাঁচটা স্টেপ দিয়ে 
+
+// ২. Not Found বা ৪০৪ টাইপের রাউট কিভাবে সেট করে 
+
+// ৩. ডায়নামিক রাউট কিভাবে সেট করে। কিভাবে ডায়নামিক রাউট এর পাথ সেট করে 
+
+// ৪. কম্পোনেন্ট এর ভিতরে গিয়ে কিভাবে ডাইনামিক রাউট এর রাউট প্যারামিটার থেকে ভ্যালু নিয়ে আসা যায়। তখন কোন হুক ইউজ করতে হয় এবং সেটা দিয়ে ডাইনামিক url তৈরি করে কোন ডাটা লোড করার থাকলে সেটা কিভাবে করা যায়। 
+
+// ৫. বাটনে ইভেন্ট হ্যান্ডলার দিয়ে কিভাবে ক্লিক করেলে রাউট সেট করে যায় 
